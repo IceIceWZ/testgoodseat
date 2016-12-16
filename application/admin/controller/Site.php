6a42b3720e49cd31952ce1ed8f8e99d801cf1ac3
IceIceWZ <1615540020@qq.com>
2016-12-16 11:15:45 +0800

    '测试1'

---
+++ application/admin/controller/Site.php
@@ -0,0 +1,40 @@
+<?php
+namespace app\admin\controller;
+use think\Controller;
+use app\admin\model\Site;
+
+class Site extends Controller
+{
+	public function set()
+	{
+		$stitle = $_REQUEST['stitle'];
+		$surl = $_REQUEST['surl'];
+		$sdescription = $_REQUEST['sdescription'];
+		$s_name = $_REQUEST['s_name'];
+		$s_phone = $_REQUEST['s_phone'];
+		$s_qq = $_REQUEST['s_qq'];
+		$s_email = $_REQUEST['s_email'];
+		$s_address = $_REQUEST['s_address'];
+		$scopyright = $_REQUEST['scopyright'];
+		$sid = $_REQUEST['sid'];
+		$site = new Site;
+		$result = $site->save([
+				'title' =>$stitle,
+				'domain_name'=>$surl,
+				'site_describe'=>$sdescription,
+				'linkman_name'=>$s_name,
+				'phone'=>$s_phone,
+				'qq'=>$s_qq,
+				'email'=>$s_address,
+				'address'=>$s_address,
+				'site_information'=>$scopyright
+
+
+			],['id'=>$sid]);
+		if($result){
+			$this->success('更新成功');
+		}else{
+			$this->error('更新失败');
+		}
+	}
+}
